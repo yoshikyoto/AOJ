69132417c5ef8e417977f606ad9bb41ae91f6396
sakamoto <admin@pc-a320.local>
2014-04-23 08:50:33 +0900

    add try-catch syntax

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,8 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while(true){
-					int n = Integer.parseInt(br.readLine());
-					if(n == 0) return;
-
-					String strs[] = br.readLine().split(" ");
-					int num = (n * (n-1)) / 2;
-					int sum = 0;
-					for(int i = 0; i < num; i++){
-						sum += Integer.parseInt(strs[i]);
-					}
-					sum = sum / (n - 1);
-					System.out.println(sum);
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 		}

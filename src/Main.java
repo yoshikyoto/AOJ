4faff27ed22fff3f3bac6e2bc5fdf1f6ed36f4a7
sakamoto <admin@pc-a320.lan>
2014-04-23 01:26:03 +0900

    1014 clear

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main{
 	public static void main(String args[]){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true){
-			return;
+		try {
+			while(true){
+					int n = Integer.parseInt(br.readLine());
+					if(n == 0) return;
+
+					String strs[] = br.readLine().split(" ");
+					int num = (n * (n-1)) / 2;
+					int sum = 0;
+					for(int i = 0; i < num; i++){
+						sum += Integer.parseInt(strs[i]);
+					}
+					sum = sum / (n - 1);
+					System.out.println(sum);
+			}
+		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 		}
 	}
 }

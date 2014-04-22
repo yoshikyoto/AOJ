350ea087d8365113555e34f41e884f7ee3a7c419
sakamoto <admin@pc-a320.local>
2014-04-22 16:45:44 +0900

    add BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class Main{
 	public static void main(String args[]){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			return;
+		}
 	}
 }

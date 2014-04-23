d70ce1b3e55b0590ce4a27d7f46f4cb37ae45d74
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-04-24 00:37:36 +0900

    v1

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,38 @@ class Main{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while(true){
+				String strs[] = br.readLine().split(" ");
+				int n = Integer.parseInt(strs[0]);
+				int h = Integer.parseInt(strs[1]);
+				if(n == 0) return;
+				
+				boolean ana[][][] = new boolean[n][n][n];
+				
+				for(int i = 0; i < h; i++){
+					strs = br.readLine().split(" ");
+					int arg1 = Integer.parseInt(strs[1]) - 1;
+					int arg2 = Integer.parseInt(strs[2]) - 1;
+					if(strs[0].equals("xy")){
+						// xy
+						for(int j = 0; j < n; j++)
+							ana[arg1][arg2][j] = true;
+					}else if(strs[0].equals("xz")){
+						// xz
+						for(int j = 0; j < n; j++)
+							ana[arg1][j][arg2] = true;
+					}else{
+						// yz
+						for(int j = 0; j < n; j++)
+							ana[j][arg1][arg2] = true;
+					}
+				}
+				
+				int answer = n * n * n;
+				for(int i = 0; i < n; i++)
+					for(int j = 0; j < n; j++)
+						for(int k = 0; k < n; k++)
+							if(ana[i][j][k]) answer--;
+				System.out.println(answer);
 			}
 		} catch (Exception e) {
 				// TODO Auto-generated catch block

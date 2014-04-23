cac6233673fab547f1d0995db543f66057575228
sakamoto <admin@pc-a320.local>
2014-04-23 09:43:06 +0900

    add printArray for int

--- src/Main.java
+++ src/Main.java
@@ -13,4 +13,11 @@ class Main{
 				e.printStackTrace();
 		}
 	}
+	
+	public static void printArray(int array[]){
+		for(int i = 0; i < array.length; i++){
+			System.out.print(array[i] + "\t");
+		}
+		System.out.println();
+	}
 }

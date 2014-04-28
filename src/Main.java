ffa1303013cefd7d67a5ea89363eae14960fce56
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-04-28 12:22:23 +0900

    time limit

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.io.InputStreamReader;
 
 class Main{
 	static int sx, sy, gx, gy;
+	
 	public static void main(String args[]){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			while(true){
 				String strs[] = br.readLine().split(" ");
-				int h = Integer.parseInt(strs[1]);
 				int w = Integer.parseInt(strs[0]);
+				int h = Integer.parseInt(strs[1]);
+				if(w == 0) return;
 				
 				int tile[][] = new int[h][w];
+				
 				for(int i = 0; i < h; i++){
 					strs = br.readLine().split(" ");
 					for(int j = 0; j < w; j++){
@@ -26,8 +29,10 @@ class Main{
 						}
 					}
 				}
-				System.out.println(sy + " " + sx + " " + gy + " " + gx);
-				System.out.println(solve(tile, sx, sy, 0));
+				//System.out.println(sy + " " + sx + " " + gy + " " + gx);
+				int ans = solve(tile, sx, sy, 0);
+				if(ans == 11) ans = -1;
+				System.out.println(ans);
 			}
 		} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -36,31 +41,31 @@ class Main{
 	}
 	
 	public static int solve(int tile[][], int x, int y, int count){
-		tile[y][x] = 2;
-		printMatrix(tile);
+		//tile[y][x] = 2;
+		// printMatrix(tile);
 		if(count >= 10) return 11;
 		int up = 11, right = 11, down = 11, left = 11;
 		// ã
 		if(y > 0 && tile[y-1][x] != 1){
-			System.out.println("ã‚É“®‚©‚¹‚é");
+			// System.out.println("ã‚É“®‚©‚¹‚é");
 			up = goUp(tile, x, y, count);
 		}
 		
 		// ‰E
 		if(x < tile[y].length-1 && tile[y][x+1] != 1){
-			System.out.println("‰E‚É“®‚©‚¹‚é");
+			// System.out.println("‰E‚É“®‚©‚¹‚é");
 			right = goRight(tile, x, y, count);
 		}
 		
 		// ‰º
 		if(y < tile.length-1 && tile[y+1][x] != 1){
-			System.out.println("‰º‚É“®‚©‚¹‚é");
+			// System.out.println("‰º‚É“®‚©‚¹‚é");
 			down = goDown(tile, x, y, count);
 		}
 		
 		// ¶
 		if(x > 0 && tile[y][x-1] != 1){
-			System.out.println("¶‚É“®‚©‚¹‚é");
+			// System.out.println("¶‚É“®‚©‚¹‚é");
 			left = goLeft(tile, x, y, count);
 		}
 		return Math.min(Math.min(up, down), Math.min(right, left));
@@ -68,14 +73,14 @@ class Main{
 	
 	public static int goDown(int tile[][], int x, int y, int count){
 		count++;
-		System.out.println("‰º‚É“®‚©‚·" + count);
+		// System.out.println("‰º‚É“®‚©‚·" + count);
 		for(; y < tile.length-1; y++){
 			if(tile[y+1][x] == 3){
-				System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
+				// System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
 				return count;
 			}
 			if(tile[y+1][x] == 1){
-				System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
+				// System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
 				tile[y+1][x] = 0;
 				int result = solve(tile, x, y, count);
 				tile[y+1][x] = 1;
@@ -87,14 +92,14 @@ class Main{
 	
 	public static int goRight(int tile[][], int x, int y, int count){
 		count++;
-		System.out.println("‰E‚É“®‚©‚·" + count);
+		// System.out.println("‰E‚É“®‚©‚·" + count);
 		for(; x < tile[y].length-1; x++){
 			if(tile[y][x+1] == 3){
-				System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
+				// System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
 				return count;
 			}
 			if(tile[y][x+1] == 1){
-				System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
+				// System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
 				tile[y][x+1] = 0;
 				int result = solve(tile, x, y, count);
 				tile[y][x+1] = 1;
@@ -106,14 +111,14 @@ class Main{
 	
 	public static int goUp(int tile[][], int x, int y, int count){
 		count++;
-		System.out.println("ã‚É“®‚©‚·" + count);
+		// System.out.println("ã‚É“®‚©‚·" + count);
 		for(; y > 0; y--){
 			if(tile[y-1][x] == 3){
-				System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
+				// System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
 				return count;
 			}
 			if(tile[y-1][x] == 1){
-				System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
+				// System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
 				tile[y-1][x] = 0;
 				int result = solve(tile, x, y, count);
 				tile[y-1][x] = 1;
@@ -125,14 +130,14 @@ class Main{
 	
 	public static int goLeft(int tile[][], int x, int y, int count){
 		count++;
-		System.out.println("¶‚É“®‚©‚·" + count);
+		// System.out.println("¶‚É“®‚©‚·" + count);
 		for(; x > 0; x--){
 			if(tile[y][x-1] == 3){
-				System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
+				// System.out.println("ƒS[ƒ‹‚Å‚·: " + count);
 				return count;
 			}
 			if(tile[y][x-1] == 1){
-				System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
+				// System.out.println("•Ç‚É‚Ô‚Â‚©‚Á‚½");
 				tile[y][x-1] = 0;
 				int result = solve(tile, x, y, count);
 				tile[y][x-1] = 1;
